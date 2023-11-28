ee0043ff58baecc671b22920316efb1ac619ff02
QlikDinara <66374926+QlikDinara@users.noreply.github.com>
2023-11-28 22:27:22 +0100

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -5,41 +5,36 @@ a = np.array([['1', '2','3'],
 print(a)
 
 def chooseX():
-  print ("X: Pick a number from 1 to 9 and make your move")
-  x=input()
-  ii = np.where(a == x)
-  xcoor=ii[0]
-  ycoor=ii[1]
-  if (a.size>0):
-    if (len(ii) == 0):
-      print("X: This cell was already picked choose another one")
-      chooseX()
-    if ((a[xcoor,ycoor]!='X') or (a[xcoor,ycoor]!='O')):
-      print('no existing')
-      a[xcoor,ycoor]='X'
-    print(a)
+    print ("X: Pick a number from 1 to 9 and make your move")
+    o=input()
+    jj = np.where(a == o)
+    x_coor=jj[0]
+    y_coor=jj[1]
+    if((int(o)>=1) or (int(o)<=9) and a.size>0):
+      if (len(x_coor) == 0 and len(y_coor)==0):
+        print("This cell was already picked or out of the range, please choose another one")
+        chooseO()
+      elif ((a[x_coor,y_coor]!='X') or (a[x_coor,y_coor]!='O')):
+        a[x_coor,y_coor]='X'
+      print(a)
     
 def chooseO():
   print ("O: Pick a number from 1 to 9 and make your move")
   o=input()
   jj = np.where(a == o)
-  print(jj)
-  x_coor=jj[0]
-  y_coor=jj[1]
   print(x_coor)
   print(y_coor)
-  if (a.size>0):
+  if((int(o)>=1) or (int(o)<=9) and a.size>0):
     if (len(x_coor) == 0 and len(y_coor)==0):
-      print("This cell was already picked choose another one")
+      print("This cell was already picked or out of the range, please choose another one")
       chooseO()
-    if ((a[x_coor,y_coor]!='X') or (a[x_coor,y_coor]!='O')):
-      print('not existing')
+    elif ((a[x_coor,y_coor]!='X') or (a[x_coor,y_coor]!='O')):
       a[x_coor,y_coor]='O'
     print(a)
 
 while True:
   for step in range(0,9):
-    #chooseX()
+    chooseX()
     chooseO()
       
      
