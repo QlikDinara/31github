1faba0ca2433d3c21e4ca9377b8f91fcc0e5467a
QlikDinara <66374926+QlikDinara@users.noreply.github.com>
2023-12-01 22:22:05 +0100

    Create databricks.py

---
+++ databricks.py
@@ -0,0 +1,10 @@
+%python
+
+diamonds = (spark.read
+  .format("csv")
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .load("/databricks-datasets/Rdatasets/data-001/csv/ggplot2/diamonds.csv")
+)
+
+diamonds.write.format("delta").mode("overwrite").save("/mnt/delta/diamonds")
