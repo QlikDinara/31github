ee2df46f656878f4c02558675b2770d4e9af7a13
QlikDinara <66374926+QlikDinara@users.noreply.github.com>
2023-12-03 02:12:11 +0100

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -2,62 +2,72 @@ import numpy as np
 a = np.array([['1', '2','3'],
               ['4', '5', '6'],
               ['7', '8', '9']])
-print(a)
 
-def chooseX():
-    print ("X: Pick a number from 1 to 9 and make your move")
-    o=input()
-    jj = np.where(a == o)
+
+
+def chooseXO(XO,counter):
+    print ("Pick a number from 1 to 9 and make your move")
+    #o=input()
+    jj = np.where(a == XO)
     x_coor=jj[0]
     y_coor=jj[1]
     if((int(o)>=1) or (int(o)<=9) and a.size>0):
       if (len(x_coor) == 0 and len(y_coor)==0):
         print("This cell was already picked or out of the range, please choose another one")
-        chooseO()
+        chooseXO(XO)
       elif ((a[x_coor,y_coor]!='X') or (a[x_coor,y_coor]!='O')):
-        a[x_coor,y_coor]='X'
+        if counter%2==1:
+            a[x_coor,y_coor]='X'
+        else:
+            a[x_coor,y_coor]='O'
       print(a)
     
 def chooseO():
   print ("O: Pick a number from 1 to 9 and make your move")
   o=input()
   jj = np.where(a == o)
-  print(x_coor)
-  print(y_coor)
+  x_coor=jj[0]
+  y_coor=jj[1]
   if((int(o)>=1) or (int(o)<=9) and a.size>0):
     if (len(x_coor) == 0 and len(y_coor)==0):
       print("This cell was already picked or out of the range, please choose another one")
       chooseO()
     elif ((a[x_coor,y_coor]!='X') or (a[x_coor,y_coor]!='O')):
       a[x_coor,y_coor]='O'
-    print(a)
-	
-	
+    print("board ",a)
+
 def determine_winner(game_board):
-    """check if there is a win, draw"""
-    # check rows
-    for i in range(3):
-        if game_board[i*3] == game_board[i*3+1] == game_board[i*3+2] and game_board[i*3] in ('X', 'O'):
-            return game_board[i*3]
-    # check columns
-    for i in range(3):
-        if game_board[i] == game_board[i+3] == game_board[i+6] and game_board[i] in ('X', 'O'):
-            return game_board[i]
-    # check diagonals
-    if game_board[0] == game_board[4] == game_board[8] and game_board[0] in ('X', 'O'):
-        return game_board[0]
-    if game_board[2] == game_board[4] == game_board[6] and game_board[2] in ('X', 'O'):
-        return game_board[2]
-    # check tie
-    if all(x in ('X', 'O') for x in game_board):
-        return 'Tie'
-    # no winner or tie
-    return False
+  winner=None
+  j=0
+  for i in range(3):
+        if a[i][j] == a[i][j+1] == a[i][j+2] and a[i][j] in ('X', 'O'):
+         # print('Winner is row',a[i][j])
+          winner=a[i][j]
+  l=0
+  for k in range(3):
+    if a[l][k] == a[l+1][k] == a[l+2][k] and a[l][k] in ('X', 'O'):
+      #print('Winner is column',a[l][k])
+      winner=a[l][k]
+  r=0
+  c=0
+  if a[r][c] == a[r+1][c+1] == a[r+2][c+2] and a[r][c] in ('X', 'O'):
+      #print('Winner is diagonal',a[r][c])
+      winner=a[r][c]
+  x=0
+  y=2
+  if a[x][y] == a[x+1][y-1] == a[x+2][y-2] and a[x][y] in ('X', 'O'):
+    #print('Winner is counter diagonal',a[x][y])
+    winner=a[x][y]
+  return winner
 
-while True:
-  for step in range(0,9):
-    chooseX()
-    chooseO()
+counter=1
+while not determine_winner(a):
+    print(a)
+    o=input()
+    chooseXO(o, counter)
+    print("Winner is ",determine_winner(a))
+    counter=counter+1
+    """chooseO()
+    determine_winner(a)"""
 
-determine_winner(a)  
      
