a589341b12c7f71c973d26b42894ab80947804d0
QlikDinara <66374926+QlikDinara@users.noreply.github.com>
2023-12-18 20:33:14 -0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,10 +1,7 @@
 print("Please enter your height in meters e.g., 1.55")
 height = float(input())
-# Enter your weight in kilograms e.g., 72
+print("Enter your weight in kilograms e.g., 72")
 weight = int(input())
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 bmi=round(float(weight/(height*height)), 2)
 if bmi < 18.5:
   print("Your BMI is",bmi,", you are underweight.")
